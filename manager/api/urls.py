8ca4c3fd8e40a29cf6a5c90360327ef0009d5502
Sergio Orbe <sergioo@exo.com.ar>
2015-06-24 17:16:26 -0300

    reporte de charlas y staff por sede

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -5,6 +5,7 @@ from manager.api.routes import router
 urlpatterns = patterns(
     '',
     url(r'^(?i)(?P<sede_url>[a-zA-Z0-9-_]+)/sede_report$', views.sede_report, name='sede report'),
+    url(r'^(?i)(?P<sede_url>[a-zA-Z0-9-_]+)/sede_report2$', views.sede_report2, name='sede report2'),
 
     # Django REST
     url(r'^', include(router.urls)),
