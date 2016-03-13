301386740edd376caa335e115ad1387eb6439e06
Sergio Orbe <reyiyo@gmail.com>
2016-03-13 15:04:44 -0300

    Revert "remove address model"
    This reverts commit cd83cfa23053973373f10c47c9f0f772c813d48d.

--- manager/api/tests/load_data.py
+++ manager/api/tests/load_data.py
@@ -4,6 +4,7 @@ import autofixture
 class Loader():
 
     def __init__(self):
+        self.adress = None
         self.events = None
         self.admins = None
         self.users = None
@@ -11,4 +12,5 @@ class Loader():
     def loadModels(self):
         self.admins = autofixture.create('auth.User', 5, field_values={'is_superuser': True})
         self.users = autofixture.create('auth.User', 30, field_values={'is_superuser': False})
+        self.adress = autofixture.create('event.Adress', 5)
         self.events = autofixture.create('event.Event', 5)
