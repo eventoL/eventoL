c0f2ce5fd8a40b0ca3da99d0c96d00a06a310554
Sergio Orbe <reyiyo@gmail.com>
2017-03-15 15:34:31 -0300

    Fix accidentally deleted url

--- eventoL/urls.py
+++ eventoL/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^create-event/$', views.create_event, name="create_event"),
     url(r'^event/', include('manager.urls'), name='event'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls'), name='ckeditor'),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='account/profile.html'), name="user_profile"),
     url(r'^accounts/', include('allauth.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
