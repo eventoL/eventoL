738dc3635527475516628ee1a55c1e2426703239
Sergio Orbe <reyiyo@gmail.com>
2017-03-15 21:30:29 -0300

    #243 Navbar refactor

--- eventoL/urls.py
+++ eventoL/urls.py
@@ -8,15 +8,17 @@ from manager import views
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^create-event/$', views.create_event, name="create_event"),
-    url(r'^event/', include('manager.urls'), name='event'),
-    url(r'^admin/', include(admin.site.urls), name='admin'),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls'), name='ckeditor'),
-    url(r'^accounts/profile/', TemplateView.as_view(template_name='account/profile.html'), name="user_profile"),
-    url(r'^accounts/', include('allauth.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'^$', views.home, name="home"),
+                  url(r'^create-event/$', views.create_event, name="create_event"),
+                  url(r'^event/', include('manager.urls'), name='event'),
+                  url(r'^admin/', include(admin.site.urls), name='admin'),
+                  url(r'^ckeditor/', include('ckeditor_uploader.urls'), name='ckeditor'),
+                  url(r'^accounts/profile/', TemplateView.as_view(template_name='account/profile.html'),
+                      name="user_profile"),
+                  url(r'^accounts/', include('allauth.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
