c2f70c401bdb491b462010ad0cadfea1761f77fc
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-11-28 11:38:48 -0300

    Create eventol folder with django proyect files

--- eventol/wsgi.py
+++ eventol/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for eventoL project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/python
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventoL.settings")
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 
-from django.core.wsgi import get_wsgi_application
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+from eventoL import wsgi
 
-application = get_wsgi_application()
+application = wsgi.application
