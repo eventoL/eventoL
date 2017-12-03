cee79e3749b36b721030b3a7a2f9cf6c60abfbd5
FedeG <federicogonzalez761@gmail.com>
2017-12-03 12:13:06 -0300

    Add front folder with react configuration and example

---
+++ eventol/front/server.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.local.config')
+const host = '0.0.0.0'
+const port = 3000
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  inline: true,
+  historyApiFallback: true,
+  headers: { 'Access-Control-Allow-Origin': '*' }
+}).listen(port, host, (err) => {
+  if (err) console.log(err);
+  console.log(`Listening at ${host}:${port}`);
+})
