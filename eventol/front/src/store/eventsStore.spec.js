58302aafe02db7325da5f244a43470ed3957b0d3
FedeG <federicogonzalez761@gmail.com>
2017-12-20 09:00:45 -0300

    Add redux store

---
+++ eventol/front/src/store/eventsStore.spec.js
@@ -0,0 +1,82 @@
+const initState = {
+  events: {
+    events: []
+  }
+}
+
+describe('Events store', () => {
+  let store, event, events;
+
+  beforeEach(() => {
+    const get_store = require('./eventsStore').default
+    store = get_store()
+    event = {
+      model: 'events.event',
+      pk: 1,
+      data: {
+        name: 'Gitlab with workshop',
+        url: 'https://gitlab.com/FedeG/django-react-workshop/',
+        pending: false,
+        description: '',
+        user: 1
+      }
+    }
+    events = [event]
+  });
+
+  test('returns a state object', () => {
+    const result = store.getState();
+    expect(result).toBeDefined();
+  });
+
+  test('returns default', () => {
+    const result = store.getState();
+    expect(result).toEqual(initState);
+  });
+
+  describe('events reducers', () => {
+
+    test('when dispatch SET_EVENTS returns correct state', () => {
+      const expectData = {events};
+      store.dispatch({type: 'SET_EVENTS', events});
+      expect(store.getState().events).toEqual(expectData);
+    });
+
+    test('when dispatch UPDATE_EVENT returns correct state', () => {
+      const expectedEvent = {fields: {name: 'new name'}, pk: 1};
+      const expectData = {events: [expectedEvent]};
+      store.dispatch({type: 'SET_EVENTS', events});
+      store.dispatch({type: 'UPDATE_EVENT', event: {
+        ...event,
+        data: { name: 'new name' },
+      }});
+      expect(store.getState().events).toEqual(expectData);
+    });
+
+    test('when dispatch DELETE_EVENT returns correct state', () => {
+      const expectData = {events: []};
+      store.dispatch({type: 'SET_EVENTS', events});
+      store.dispatch({type: 'DELETE_EVENT', event});
+      expect(store.getState().events).toEqual(expectData);
+    });
+
+    test('when dispatch CREATE_EVENT returns correct state', () => {
+      const createdEvent = {
+       fields: {
+          description: '',
+          name: 'Gitlab with workshop',
+          pending: false,
+          url: 'https://gitlab.com/FedeG/django-react-workshop/',
+          user: 1,
+        },
+        pk: 1,
+      }
+      const expectData = {events: [event, createdEvent]};
+      store.dispatch({type: 'SET_EVENTS', events});
+      store.dispatch({type: 'CREATE_EVENT', event});
+      expect(store.getState().events).toEqual(expectData);
+    });
+
+  });
+
+})
