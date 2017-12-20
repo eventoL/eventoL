ef80597b20535908cfcbf0463adce156ab665552
FedeG <federicogonzalez761@gmail.com>
2017-12-20 08:55:58 -0300

    Add UserProfile component

---
+++ eventol/front/src/components/UserProfile/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+
+const UserProfile = () => (
+  <div className="UserProfile">
+    <div className="User">
+      <div className="name">Federico Gonzalez</div>
+      <div className="image">
+        <img src="https://lh3.googleusercontent.com/-L9sUzpq9dXo/AAAAAAAAAAI/AAAAAAAAAAA/AFiYof3VquGCDqIIOgcCoqRuuAYGPX7mlQ/s32-c-mo/photo.jpg" alt="profile" />
+      </div>
+    </div>
+  </div>
+);
+
+export default UserProfile;
