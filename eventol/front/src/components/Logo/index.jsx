165dc62eb2c8c4e9a975395a2e1166dd3d8b1757
FedeG <federicogonzalez761@gmail.com>
2017-12-20 08:58:35 -0300

    Add Logo component

---
+++ eventol/front/src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import eventolLogo from '../../assets/imgs/eventol-white.png'
+import './index.css';
+
+
+const Logo = () => (
+	<div id="logo" className="Logo">
+	  <img src={eventolLogo} />
+  </div>
+);
+
+export default Logo;
