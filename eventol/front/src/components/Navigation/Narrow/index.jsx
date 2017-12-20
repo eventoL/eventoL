248adc0b28df39be3ce00a577d629cca2375f227
FedeG <federicogonzalez761@gmail.com>
2017-12-20 08:55:37 -0300

    Add Navigation component

---
+++ eventol/front/src/components/Navigation/Narrow/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+
+const Navigation = () => (
+  <div id="navigation" className="NavigationNarrow">
+    <nav>
+      <ul>
+        <li>Buscar</li>
+        <li>Mi lista</li>
+        <li>Destacados</li>
+        <li>Recientes</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navigation;
---
+++ eventol/front/src/components/Navigation/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+
+const Navigation = () => (
+  <div id="navigation" className="Navigation">
+    <nav>
+      <ul>
+        <li>Buscar</li>
+        <li>Mi lista</li>
+        <li>Destacados</li>
+        <li>Recientes</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navigation;
