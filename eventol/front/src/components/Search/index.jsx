5878fde1db6e0d87fb38e3db4f93b4bbb986aa3e
FedeG <federicogonzalez761@gmail.com>
2017-12-20 08:58:47 -0300

    Add Search component

---
+++ eventol/front/src/components/Search/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './index.css';
+
+
+export default class Search extends React.Component {
+  state = {
+    searchTerm: ''
+  }
+
+  handleKeyUp = event => {
+    if (event.key === 'Enter') {
+      const {onEnter} = this.props;
+      if (onEnter) onEnter();
+    }
+  }
+
+  handleChange = event => {
+    const searchTerm = event.target.value;
+    this.setState({searchTerm});
+    const {onChange} = this.props;
+    if (onChange) onChange(searchTerm);
+  }
+
+  render(){
+    return (
+      <div id='search' className='Search'>
+        <input
+          onKeyUp={this.handleKeyUp}
+          onChange={this.handleChange}
+          type='search'
+          placeholder='Buscar por titulo...'
+          value={this.state.searchTerm}/>
+      </div>
+    );
+  }
+}
