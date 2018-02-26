d40be1915d682e6a8e4e7abe6fc96a08319a4e0a
FedeG <federicogonzalez761@gmail.com>
2018-02-26 09:51:54 -0300

    Add link to account profile

--- eventol/front/src/components/UserProfile/Narrow/index.jsx
+++ eventol/front/src/components/UserProfile/Narrow/index.jsx
@@ -4,12 +4,14 @@ import './index.css';
 
 const UserProfile = () => (
   <div className="UserProfileNarrow">
-    <div className="User">
-      <div className="name">Federico Gonzalez</div>
-      <div className="image">
-        <img src="https://lh3.googleusercontent.com/-L9sUzpq9dXo/AAAAAAAAAAI/AAAAAAAAAAA/AFiYof3VquGCDqIIOgcCoqRuuAYGPX7mlQ/s32-c-mo/photo.jpg" alt="profile" />
+    <a href='/accounts/profile/' style={{textDecoration: 'none'}}>
+      <div className="User">
+        <div className="name">Federico Gonzalez</div>
+        <div className="image">
+          <img src="https://lh3.googleusercontent.com/-L9sUzpq9dXo/AAAAAAAAAAI/AAAAAAAAAAA/AFiYof3VquGCDqIIOgcCoqRuuAYGPX7mlQ/s32-c-mo/photo.jpg" alt="profile" />
+        </div>
       </div>
-    </div>
+    </a>
   </div>
 );
 
