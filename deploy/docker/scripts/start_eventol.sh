85217946ff6f7acb8e316f3e6f4e7c92a3e01d58
FedeG <federicogonzalez761@gmail.com>
2018-03-16 23:59:43 -0300

    Add loaddata for default user and social apps

--- deploy/docker/scripts/start_eventol.sh
+++ deploy/docker/scripts/start_eventol.sh
@@ -4,7 +4,9 @@ cd eventol
 python manage.py makemigrations manager --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py loaddata manager/initial_data/admin.json
 python manage.py loaddata manager/initial_data/attendee_data.json
 python manage.py loaddata manager/initial_data/email_addresses.json
 python manage.py loaddata manager/initial_data/initial_data.json
+python manage.py loaddata manager/initial_data/social.json
 python manage.py runworker
