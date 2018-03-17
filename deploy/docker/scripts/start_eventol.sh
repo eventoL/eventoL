6444d58d5b976f1bf2223d1a81634e113a462a8b
FedeG <federicogonzalez761@gmail.com>
2018-03-17 00:21:05 -0300

    Add import for sites in database load

--- deploy/docker/scripts/start_eventol.sh
+++ deploy/docker/scripts/start_eventol.sh
@@ -8,5 +8,6 @@ python manage.py loaddata manager/initial_data/admin.json
 python manage.py loaddata manager/initial_data/attendee_data.json
 python manage.py loaddata manager/initial_data/email_addresses.json
 python manage.py loaddata manager/initial_data/initial_data.json
+python manage.py loaddata manager/initial_data/sites.json
 python manage.py loaddata manager/initial_data/social.json
 python manage.py runworker
