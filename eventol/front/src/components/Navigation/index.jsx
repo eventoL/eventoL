50c5b384e49f88eec720e4ef4d986ba166c930ae
FedeG <federicogonzalez761@gmail.com>
2018-03-18 18:05:41 -0300

    close #261 - add link to documentation in home page

--- eventol/front/src/components/Navigation/index.jsx
+++ eventol/front/src/components/Navigation/index.jsx
@@ -13,9 +13,9 @@ export default class Navigation extends React.Component {
         <nav>
           <ul>
             <li onClick={this.searchFocus}>{gettext('Search')}</li>
+            <a href='http://eventol.github.io/eventoL/#/'><li>{gettext('Documentation')}</li></a>
             <a href='#featured'><li>{gettext('Featured')}</li></a>
             <a href='#recent'><li>{gettext('Recent')}</li></a>
-            <a href='http://eventol.github.io/eventoL/#/'><li>{gettext('Documentation')}</li></a>
           </ul>
         </nav>
       </div>
