142f32c5d26f19cbfd7a605012a1bd95812a9a81
FedeG <federicogonzalez761@gmail.com>
2018-03-21 20:09:07 -0300

    Fix email backend

--- eventol/eventol/settings.py
+++ eventol/eventol/settings.py
@@ -263,7 +263,7 @@ class Base(Configuration):
         },
     }
 
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
     EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.unset')
     EMAIL_PORT = os.getenv('EMAIL_PORT', '587')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'change_unset@mail.com')
