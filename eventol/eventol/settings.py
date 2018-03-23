2b516ad2f920698cfba5f64452cc8279952ba0e8
root <root@eventol>
2018-03-22 22:28:06 -0300

    Update settings for media files

--- eventol/eventol/settings.py
+++ eventol/eventol/settings.py
@@ -401,6 +401,10 @@ class Staging(Base):
             )
         }
     }
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_URL = '/static/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
 
 
 class Prod(Staging):
