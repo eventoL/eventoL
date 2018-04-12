4c825bb94a457a83140f9e35dc09caa0c5744cfd
FedeG <federicogonzalez761@gmail.com>
2018-04-12 18:32:01 -0300

    #266 - Refactor in testing

--- eventol/manager/tests/test_count_by.py
+++ eventol/manager/tests/test_count_by.py
@@ -15,20 +15,25 @@ class TestTagFilters(unittest.TestCase):
         def increment(element):
             raise Exception(element)
         lista = [{'b': False}] * 4
-        self.assertDictEqual(count_by(lista, lambda n: n, increment=increment), {})
+        self.assertDictEqual(
+            count_by(lista, lambda n: n, increment=increment), {})
 
     def test_count_by_if_count_for_key_return_key_count(self):
         lista = [{'b': 1}] * 4 + [{'b': 2}] * 4
-        self.assertDictEqual(count_by(lista, lambda el: el['b']), {1: 4, 2: 4})
+        self.assertDictEqual(
+            count_by(lista, lambda el: el['b']), {1: 4, 2: 4})
 
     def test_count_by_if_count_for_key_and_increment_with_value_return_key_count_incremented(self):
         lista = [{'b': 1}] * 4 + [{'b': 2}] * 4
-        self.assertDictEqual(count_by(lista, lambda el: el['b'], lambda el: el['b']), {1: 4, 2: 8})
+        self.assertDictEqual(
+            count_by(lista, lambda el: el['b'], lambda el: el['b']), {1: 4, 2: 8})
 
     def test_count_by_use_with_list_return_count(self):
         lista = [2] * 5 + [1] * 4
-        self.assertDictEqual(count_by(lista, lambda n: n), {1: 4, 2: 5})
+        self.assertDictEqual(
+            count_by(lista, lambda n: n), {1: 4, 2: 5})
 
     def test_count_by_use_with_list_and_increment_with_value_return_count_incremented(self):
         lista = [2] * 5 + [1] * 4
-        self.assertDictEqual(count_by(lista, lambda n: n, lambda n: n), {1: 4, 2: 10})
+        self.assertDictEqual(
+            count_by(lista, lambda n: n, lambda n: n), {1: 4, 2: 10})
