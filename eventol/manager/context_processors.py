7e79cfa3e774927474be70946241098a21e0477c
FedeG <federicogonzalez761@gmail.com>
2018-08-12 23:17:37 -0300

    Add EVENTOL_SETTINGS to context and add to react params

--- eventol/manager/context_processors.py
+++ eventol/manager/context_processors.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from manager.models import EventolSetting
 
 
 def eventol_settings(request):
     """Add eventol settings"""
     return {
-        "PRIVATE_ACTIVITIES": settings.PRIVATE_ACTIVITIES
+        "PRIVATE_ACTIVITIES": settings.PRIVATE_ACTIVITIES,
+        "EVENTOL_SETTINGS": EventolSetting.load()
     }
