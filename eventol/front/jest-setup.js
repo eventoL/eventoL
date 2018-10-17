8ae1b8a1aff24a744dc9656d38e511b41c3d633b
FedeG <federicogonzalez761@gmail.com>
2018-10-17 20:21:55 -0300

    Fix eslints in jest-setup file

--- eventol/front/jest-setup.js
+++ eventol/front/jest-setup.js
@@ -1,9 +1,9 @@
-import { WebSocket } from 'mock-socket'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme';
+import {WebSocket} from 'mock-socket';
+import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({adapter: new Adapter()});
 
-global.WebSocket = WebSocket
-global.gettext = jest.fn(text => text)
-global.$ = jest.fn()
+global.$ = jest.fn();
+global.WebSocket = WebSocket;
+global.gettext = jest.fn(text => text);
