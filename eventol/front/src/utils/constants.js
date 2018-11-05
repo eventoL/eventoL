9bee7dc5c2fcb2b666861865d6f1fcd41a86cdf8
FedeG <federicogonzalez761@gmail.com>
2018-11-05 11:59:07 -0300

    #433 - Create frontend constants

--- eventol/front/src/utils/constants.js
+++ eventol/front/src/utils/constants.js
@@ -1,3 +1,10 @@
+export const MOBILE_WIDTH = 950;
+export const LOGO_LANDING_DEFAULT = '/static/manager/img/logo.png';
+export const BACKGROUND_DEFAULT = '/static/manager/img/background.png';
+export const LOGO_HEADER_DEFAULT = '/static/manager/img/eventol-white.png';
+export const REPORT_REQUIRED_FIELDS = 'name,event_slug,email,location,report,id,tags';
+export const HOME_REQUIRED_FIELDS = 'event_slug,place,image,name,attendees_count,abstract,tags';
+
 export const SLIDER_BASE_SETTINGS = {
   variableWidth: false,
   dots: true,
@@ -29,6 +36,3 @@ export const SLIDER_BASE_SETTINGS = {
     },
   }],
 };
-
-export const HOME_REQUIRED_FIELDS = 'event_slug,place,image,name,attendees_count,abstract,tags';
-export const REPORT_REQUIRED_FIELDS = 'name,event_slug,email,location,report,id,tags';
