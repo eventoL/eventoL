7310813b28b24c31c3c5bb5259870d704150a789
FedeG <federicogonzalez761@gmail.com>
2018-11-05 12:00:00 -0300

    #430 - Add snapshot tests to EventHome container and Home container

--- eventol/front/src/containers/EventHome/index.spec.jsx
+++ eventol/front/src/containers/EventHome/index.spec.jsx
@@ -1,34 +1,159 @@
+jest.mock('react-sizes', () => () => Component => props => <Component {...props} />);
+
+jest.mock('../../components/Hero', () => 'Hero');
+jest.mock('../../components/Header', () => 'Header');
+jest.mock('../../components/Search', () => 'Search');
+jest.mock('../../components/TitleList', () => 'TitleList');
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import EventHome from '.';
 
+
 describe('EventHome', () => {
   let component, tree, background,
-    logoHeader, logoLanding, tagMessage,
-    tagSlug, user;
+    logoHeader, logoLanding, tagSlug,
+    tagMessage, user, props;
+
+  const getComponent = allProps => {
+    component = renderer.create(<EventHome {...allProps} />);
+    return component;
+  };
 
   beforeEach(() => {
-    background = '';
-    logoHeader = '';
-    logoLanding = '';
-    tagMessage = '';
-    tagSlug = '';
-    user = {};
-    component = renderer.create(
-      <EventHome
-        background={background}
-        logoHeader={logoHeader}
-        logoLanding={logoLanding}
-        tagMessage={tagMessage}
-        tagSlug={tagSlug}
-        user={user}
-      />,
-    );
-    tree = component.toJSON();
+    tagSlug = 'tagSlug';
+    background = 'background';
+    logoHeader = 'logoHeader';
+    logoLanding = 'logoLanding';
+    tagMessage = 'eventolMessage';
+    /* eslint-disable camelcase */
+    user = {
+      first_name: 'first_name',
+      last_name: 'last_name',
+    };
+    /* eslint-enable camelcase */
+    props = {
+      user,
+      tagSlug,
+      background,
+      logoHeader,
+      logoLanding,
+      tagMessage,
+    };
   });
 
-  test('Snapshot', () => {
-    expect(tree).toMatchSnapshot();
+  describe('With user', () => {
+    describe('Desktop', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({user, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          component = getComponent({...props, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('Mobile', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({user, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          component = getComponent({...props, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+  });
+
+  describe('Without user', () => {
+    describe('Desktop', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          props = {
+            tagSlug,
+            tagMessage,
+            background,
+            logoHeader,
+            logoLanding,
+          };
+          component = getComponent({...props, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('Mobile', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          props = {
+            tagSlug,
+            tagMessage,
+            background,
+            logoHeader,
+            logoLanding,
+          };
+          component = getComponent({...props, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
   });
 });
--- eventol/front/src/containers/Home/index.spec.jsx
+++ eventol/front/src/containers/Home/index.spec.jsx
@@ -1,32 +1,155 @@
+jest.mock('react-sizes', () => () => Component => props => <Component {...props} />);
+
+jest.mock('../../components/Hero', () => 'Hero');
+jest.mock('../../components/Header', () => 'Header');
+jest.mock('../../components/Search', () => 'Search');
+jest.mock('../../components/TitleList', () => 'TitleList');
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import Home from '.';
 
-describe('EventHome', () => {
+
+describe('Home', () => {
   let component, tree, background,
     logoHeader, logoLanding,
-    eventolMessage, user;
+    eventolMessage, user, props;
+
+  const getComponent = allProps => {
+    component = renderer.create(<Home {...allProps} />);
+    return component;
+  };
 
   beforeEach(() => {
-    background = '';
-    logoHeader = '';
-    logoLanding = '';
-    eventolMessage = '';
-    user = {};
-    component = renderer.create(
-      <Home
-        background={background}
-        logoHeader={logoHeader}
-        logoLanding={logoLanding}
-        eventolMessage={eventolMessage}
-        user={user}
-      />,
-    );
-    tree = component.toJSON();
+    background = 'background';
+    logoHeader = 'logoHeader';
+    logoLanding = 'logoLanding';
+    eventolMessage = 'eventolMessage';
+    /* eslint-disable camelcase */
+    user = {
+      first_name: 'first_name',
+      last_name: 'last_name',
+    };
+    /* eslint-enable camelcase */
+    props = {
+      user,
+      background,
+      logoHeader,
+      logoLanding,
+      eventolMessage,
+    };
   });
 
-  test('Snapshot', () => {
-    expect(tree).toMatchSnapshot();
+  describe('With user', () => {
+    describe('Desktop', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({user, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          component = getComponent({...props, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('Mobile', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({user, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          component = getComponent({...props, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+  });
+
+  describe('Without user', () => {
+    describe('Desktop', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          props = {
+            eventolMessage,
+            background,
+            logoHeader,
+            logoLanding,
+          };
+          component = getComponent({...props, isMobile: false});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('Mobile', () => {
+      describe('Default', () => {
+        beforeEach(() => {
+          component = getComponent({isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+
+      describe('With custom data', () => {
+        beforeEach(() => {
+          props = {
+            eventolMessage,
+            background,
+            logoHeader,
+            logoLanding,
+          };
+          component = getComponent({...props, isMobile: true});
+          tree = component.toJSON();
+        });
+
+        test('Snapshot', () => {
+          expect(tree).toMatchSnapshot();
+        });
+      });
+    });
   });
 });
