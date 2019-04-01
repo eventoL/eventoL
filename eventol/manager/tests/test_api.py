94624b940c922389c4f6f00eb0522a377b1d7ada
FedeG <federicogonzalez761@gmail.com>
2019-04-01 19:33:46 -0300

    Use new constants in api tests

--- eventol/manager/tests/test_api.py
+++ eventol/manager/tests/test_api.py
@@ -1,6 +1,19 @@
+from django.test import Client
+from django.core.urlresolvers import reverse
+from rest_framework.test import RequestsClient, APIRequestFactory
+
 import pytest
 
-from rest_framework.test import RequestsClient, APIRequestFactory
+from .constants import ALL_API_URL_NAMES
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize('url_name', ALL_API_URL_NAMES)
+def test_anonymous_user_should_see_all_api_endpoints(url_name):
+    client = Client()
+    endpoint = reverse(url_name)
+    response = client.get(endpoint)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db(transaction=True)
