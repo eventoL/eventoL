acb93a52cc8f807148b459a89f24b48fc95807e5
FedeG <federicogonzalez761@gmail.com>
2019-06-09 17:50:57 -0300

    Update test with event data

--- eventol/manager/tests/test_api.py
+++ eventol/manager/tests/test_api.py
@@ -14,7 +14,7 @@ def test_anonymous_user_should_see_all_api_endpoints(url_name, web_client):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_get_event(api_request_factory, api_client):
+def test_get_event(api_request_factory, api_client, event1):
     request = api_request_factory.get('/api/events/', format='json')
     url = request.get_raw_uri()
     response = api_client.get(url)
@@ -22,7 +22,7 @@ def test_get_event(api_request_factory, api_client):
     assert response.status_code == 200
 
     json = response.json()
-    assert json['count'] == 0
+    assert json['count'] == 1
     assert json['next'] is None
     assert json['previous'] is None
     assert json['results'] == []
@@ -30,7 +30,7 @@ def test_get_event(api_request_factory, api_client):
 
 @pytest.mark.parametrize('api_reverse_name, query_params', ALL_API_URLS_REQUIRED_FROM_PAGE)
 @pytest.mark.django_db(transaction=True)
-def test_index_query(api_reverse_name, query_params, api_request_factory, api_client):
+def test_index_query(api_reverse_name, query_params, api_request_factory, api_client, event_data1):
     endpoint = reverse(api_reverse_name)
     url = '{}?{}'.format(endpoint, query_params)
     request = api_request_factory.get(url, format='json')
