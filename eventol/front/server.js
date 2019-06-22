dc5029b216f2cb7426b0cb861c9e116d36eac454
FedeG <federicogonzalez761@gmail.com>
2019-06-22 20:07:16 -0300

    Update eslint settings and fix babel and server linters

--- eventol/front/server.js
+++ eventol/front/server.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console,import/no-extraneous-dependencies */
 
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
