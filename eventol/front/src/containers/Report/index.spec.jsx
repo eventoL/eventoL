c384b6154835973d1a131053b27e3c717a9b15e3
FedeG <federicogonzalez761@gmail.com>
2019-08-10 12:34:10 -0300

    Fix #566 - Update tests of Report container

--- eventol/front/src/containers/Report/index.spec.jsx
+++ eventol/front/src/containers/Report/index.spec.jsx
@@ -12,168 +12,29 @@ import renderer from 'react-test-renderer';
 // jest.mock('../../utils/logger', () => ({error: jest.fn()}));
 
 import Report from '.';
+import {eventsData, eventsPrivateData} from '../../utils/__mock__/report';
 
 describe('Report', () => {
   let tree;
   let element;
-  let eventData;
   let component;
   let communicator;
 
   beforeEach(() => {
-    eventData = {
-      name: 'Alias cupidie',
-      tags: [
-        {
-          name: 'qwertyui',
-          slug: 'qwertyui',
-        },
-      ],
-      report: {
-        event_user: {
-          total: 4,
-          confirmed: 0,
-          not_confirmed: 4,
-        },
-        collaborator: {
-          total: 1,
-          confirmed: 0,
-          not_confirmed: 1,
-        },
-        organizer: {
-          total: 2,
-          confirmed: 0,
-          not_confirmed: 2,
-        },
-        attendee: {
-          with_event_user: {
-            total: 1,
-            confirmed: 2,
-            not_confirmed: -1,
-          },
-          without_event_user: {
-            total: 5,
-            confirmed: 2,
-            not_confirmed: 3,
-          },
-        },
-        installer: {
-          total: 1,
-          confirmed: 0,
-          not_confirmed: 1,
-        },
-        activity: {
-          level_count: {
-            '2': 2,
-            '3': 2,
-          },
-          status_count: {
-            '1': 1,
-            '2': 3,
-          },
-          type_count: {
-            Talk: 4,
-          },
-          confirmed: 3,
-          not_confirmed: 1,
-          total: 4,
-        },
-        installation: {
-          hardware_count: {
-            '1234': 1,
-            asd: 1,
-          },
-          software_count: {
-            ubuntu: 1,
-            ';laskdas;dlk': 1,
-          },
-          installer_count: {
-            '23': 2,
-          },
-          total: 2,
-        },
-        speakers: 3,
-      },
-      event_slug: 'qwertyui',
-      id: 18,
-      location: ['San Nicolas', 'Comuna 1', 'Buenos Aires', 'Argentina'],
-    };
-
     communicator = {
       addOnMessage: jest.fn(),
     };
 
-    const allData = {
-      report: {
-        event_user: {
-          total: 4,
-          confirmed: 0,
-          not_confirmed: 4,
-        },
-        collaborator: {
-          total: 1,
-          confirmed: 0,
-          not_confirmed: 1,
-        },
-        organizer: {
-          total: 2,
-          confirmed: 0,
-          not_confirmed: 2,
-        },
-        attendee: {
-          with_event_user: {
-            total: 1,
-            confirmed: 2,
-            not_confirmed: -1,
-          },
-          without_event_user: {
-            total: 5,
-            confirmed: 2,
-            not_confirmed: 3,
-          },
-        },
-        installer: {
-          total: 1,
-          confirmed: 0,
-          not_confirmed: 1,
-        },
-        activity: {
-          level_count: {
-            '2': 2,
-            '3': 2,
-          },
-          status_count: {
-            '1': 1,
-            '2': 3,
-          },
-          type_count: {
-            Talk: 4,
-          },
-          confirmed: 3,
-          not_confirmed: 1,
-          total: 4,
-        },
-        installation: {
-          hardware_count: {
-            '1234': 1,
-            asd: 1,
-          },
-          software_count: {
-            ubuntu: 1,
-            ';laskdas;dlk': 1,
-          },
-          installer_count: {
-            '23': 2,
-          },
-          total: 2,
-        },
-        speakers: 3,
-      },
-    };
-    fetch.mockResponseOnce(JSON.stringify({count: 1, results: [eventData]}));
-    fetch.mockResponseOnce(JSON.stringify({results: [allData]}));
+    fetch.mockResponseOnce(
+      JSON.stringify({count: eventsData.length, results: eventsData})
+    );
 
-    element = <Report communicator={communicator} />;
+    element = (
+      <Report
+        communicator={communicator}
+        eventsPrivateData={eventsPrivateData}
+      />
+    );
     component = renderer.create(element);
     tree = component.toJSON();
   });
