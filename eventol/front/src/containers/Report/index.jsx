3153e8839e14b1fdc781c7cd2b05a10192243c32
FedeG <federicogonzalez761@gmail.com>
2019-08-10 12:33:24 -0300

    #566 - Update report container and report table component

--- eventol/front/src/containers/Report/index.jsx
+++ eventol/front/src/containers/Report/index.jsx
@@ -6,16 +6,16 @@ import Toggle from 'react-input-toggle';
 import Logger from '../../utils/logger';
 import Title from '../../components/Title';
 import Button from '../../components/Button';
-import TableReport from '../../components/ReportTable';
+import ReportTable from '../../components/ReportTable';
 import ExportButton from '../../components/ExportButton';
 
-import {getUrl} from '../../utils/api';
-import {REPORT_REQUIRED_FIELDS} from '../../utils/constants';
+import {loadReports} from '../../utils/api';
+import {parseTotals, parseEvent} from '../../utils/report';
 
 import './react-input-toggle.css';
 import 'react-table/react-table.css';
 
-export default class Report extends React.Component {
+export default class Report extends React.PureComponent {
   static propTypes = {
     communicator: PropTypes.shape({
       addOnMessage: PropTypes.func.isRequired,
@@ -28,8 +28,6 @@ export default class Report extends React.Component {
   };
 
   state = {
-    allData: [],
-    columns: {},
     table: 'confirmed',
     count: 0,
     autoupdate: false,
@@ -42,178 +40,37 @@ export default class Report extends React.Component {
   componentDidMount() {
     const {communicator} = this.props;
     communicator.addOnMessage(this.updateTable);
-    let autoupdate = localStorage.getItem('autoupdate');
-    if (_.isNull(autoupdate)) autoupdate = false;
-    else autoupdate = JSON.parse(autoupdate);
-    this.setState({loading: true, autoupdate});
-    const url = '/api/events/?limit=5000&offset=0&fields=report';
-    return getUrl(url)
-      .then(({results: allData}) => {
-        console.log('getUrl results allData', allData);
-        return this.setState({
-          allData,
-          totals: this.parseTotals(allData),
-          loading: false,
-        });
-      })
-      .catch(err => Logger.error(gettext('There has been an error'), err));
+    this.loadToggleAutoupdate();
   }
 
-  loadContent = (pageSize, page, sorted) => {
-    const offset = page * pageSize;
-    let queryParams = `limit=${pageSize}&offset=${offset}&fields=${REPORT_REQUIRED_FIELDS}`;
-    if (sorted && sorted.length > 0) {
-      const [{id: name, desc}] = sorted;
-      queryParams += `&ordering=${desc ? '-' : ''}${name}`;
-    }
-    const url = `/api/events/?${queryParams}`;
-    return getUrl(url);
-  };
-
-  parseInstallationSoftwares = allData => {
-    const softwares = new Set();
-    allData.forEach(event =>
-      Object.keys(event.report.installation.software_count).forEach(key =>
-        softwares.add(key)
-      )
-    );
-    const installationsSoftwares = {};
-    softwares.forEach(software => {
-      const sum = _.sumBy(
-        allData,
-        `report.installation.software_count.${software}`
-      );
-      installationsSoftwares[software] = sum;
-    });
-    return installationsSoftwares;
-  };
-
-  parseActivitiesDetails = allData => {
-    const status = new Set();
-    const types = new Set();
-    const levels = new Set();
-    allData.forEach(event => {
-      Object.keys(event.report.activity.status_count).forEach(key =>
-        status.add(key)
-      );
-      Object.keys(event.report.activity.type_count).forEach(key =>
-        types.add(key)
-      );
-      Object.keys(event.report.activity.level_count).forEach(key =>
-        levels.add(key)
-      );
-    });
-    const activityDetail = {types: {}, status: {}, levels: {}};
-    status.forEach(element => {
-      const sum = _.sumBy(allData, `report.activity.status_count.${element}`);
-      activityDetail.status[element] = sum;
-    });
-    types.forEach(type => {
-      const sum = _.sumBy(allData, `report.activity.type_count.${type}`);
-      activityDetail.types[type] = sum;
-    });
-    levels.forEach(level => {
-      const sum = _.sumBy(allData, `report.activity.level_count.${level}`);
-      activityDetail.levels[level] = sum;
-    });
-    return activityDetail;
-  };
-
-  parseTotals = allData => ({
-    speakers: _.sumBy(allData, 'report.speakers'),
-    attendees: {
-      confirmed:
-        _.sumBy(allData, 'report.attendee.with_event_user.confirmed') +
-        _.sumBy(allData, 'report.attendee.without_event_user.confirmed'),
-      not_confirmed:
-        _.sumBy(allData, 'report.attendee.with_event_user.not_confirmed') +
-        _.sumBy(allData, 'report.attendee.without_event_user.not_confirmed'),
-      total:
-        _.sumBy(allData, 'report.attendee.with_event_user.total') +
-        _.sumBy(allData, 'report.attendee.without_event_user.total'),
-    },
-    organizers: {
-      confirmed: _.sumBy(allData, 'report.organizer.confirmed'),
-      not_confirmed: _.sumBy(allData, 'report.organizer.not_confirmed'),
-      total: _.sumBy(allData, 'report.organizer.total'),
-    },
-    collaborators: {
-      confirmed: _.sumBy(allData, 'report.collaborator.confirmed'),
-      not_confirmed: _.sumBy(allData, 'report.collaborator.not_confirmed'),
-      total: _.sumBy(allData, 'report.collaborator.total'),
-    },
-    installers: {
-      confirmed: _.sumBy(allData, 'report.installer.confirmed'),
-      not_confirmed: _.sumBy(allData, 'report.installer.not_confirmed'),
-      total: _.sumBy(allData, 'report.installer.total'),
-    },
-    activities: {
-      confirmed: _.sumBy(allData, 'report.activity.confirmed'),
-      not_confirmed: _.sumBy(allData, 'report.activity.not_confirmed'),
-      total: _.sumBy(allData, 'report.activity.total'),
-      details: this.parseActivitiesDetails(allData),
-    },
-    installations: {
-      total: _.sumBy(allData, 'report.installation.total'),
-      softwares: this.parseInstallationSoftwares(allData),
-    },
-  });
-
-  parseEvent = event => {
-    const {eventsPrivateData} = this.props;
-    const {
-      location,
-      report: {attendee},
-    } = event;
-    const eventData = {
-      locationDetail: {
-        address_detail: location.slice(0, -3).join(' '),
-        address: location[location.length - 3],
-        province: location[location.length - 2],
-      },
-      assistanceDetail: {
-        attendees: {
-          confirmed:
-            attendee.with_event_user.confirmed +
-            attendee.without_event_user.confirmed,
-          not_confirmed:
-            attendee.with_event_user.not_confirmed +
-            attendee.without_event_user.not_confirmed,
-          total:
-            attendee.with_event_user.total + attendee.without_event_user.total,
-        },
-      },
-      ...event,
-    };
-    if (!_.isEmpty(eventsPrivateData)) {
-      const privateData = _.find(eventsPrivateData, {id: event.id});
-      if (!_.isEmpty(privateData)) return {...privateData, ...eventData};
-    }
-    return eventData;
-  };
-
   fetchData = ({pageSize, page, sorted, filtered}) => {
-    this.setState({loading: true});
-    this.loadContent(pageSize, page, sorted, filtered)
+    const {eventsPrivateData} = this.props;
+    loadReports(pageSize, page, sorted, filtered)
       .then(({count, results}) => {
-        console.log('fetchData count', count);
-        console.log('fetchData results', results);
-        // Now just get the rows of data to your React Table
-        //   (and update anything else like total pages or loading)
         const quotient = Math.floor(count / pageSize);
         const remainder = count % pageSize;
         const pages = remainder > 0 ? quotient + 1 : quotient;
+        const data = results.map(event => parseEvent(event, eventsPrivateData));
+        const totals = parseTotals(results);
         this.setState({
-          data: results.map(this.parseEvent),
-          loading: false,
           count,
+          data,
+          loading: false,
           pages,
+          totals,
         });
       })
       .catch(err => Logger.error(gettext('There has been an error'), err));
   };
 
-  onClick = name => this.setState({table: name});
+  onClick = table => this.setState({table});
+
+  loadToggleAutoupdate = () => {
+    let autoupdate = localStorage.getItem('autoupdate');
+    if (_.isNull(autoupdate)) autoupdate = false;
+    else autoupdate = JSON.parse(autoupdate);
+    this.setState({autoupdate});
+  };
 
   handleToggleAutoupdate = () => {
     const {autoupdate} = this.state;
@@ -223,7 +80,7 @@ export default class Report extends React.Component {
 
   updateTable = () => {
     const {autoupdate} = this.state;
-    if (autoupdate) location.reload();
+    if (autoupdate) window.location.reload();
   };
 
   render() {
@@ -265,21 +122,23 @@ export default class Report extends React.Component {
             type="success"
           />
           <ExportButton
-            ref={exportButton => (this.exportButton = exportButton)}
+            ref={exportButton => {
+              this.exportButton = exportButton;
+            }}
             data={data}
             filename={table}
             label={gettext('Export')}
             type="success"
           />
         </Title>
-        <TableReport
+        <ReportTable
           count={count}
           data={data}
           defaultRows={15}
           eventsPrivateData={eventsPrivateData}
           exportButton={this.exportButton}
           fetchData={this.fetchData}
-          loading={loading}
+          isLoading={loading}
           pages={pages}
           table={table}
           totals={totals}
