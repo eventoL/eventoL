f5136be2f66e67f72fceb2ffbff6c9d6153a87df
FedeG <federicogonzalez761@gmail.com>
2019-08-26 00:31:46 -0300

    #430 - Create mock files for report data

---
+++ eventol/front/src/utils/__mock__/data.js
@@ -0,0 +1,38 @@
+export const event1 = {
+  attendees: 10,
+  backdrop: 'event1backdrop',
+  eventSlug: 'event1slug',
+  overview: 'event1overview',
+  place: 'event1place',
+  tags: [
+    {
+      name: 'slug-name',
+      slug: 'slug-slug',
+    },
+  ],
+  title: 'event 1',
+  url: 'event1Url',
+  id: 1,
+};
+
+export const event2 = {
+  attendees: 25,
+  backdrop: 'event2backdrop',
+  eventSlug: 'event2slug',
+  overview: 'event2overview',
+  place: 'event2place',
+  tags: [
+    {
+      name: 'slug-name',
+      slug: 'slug-slug',
+    },
+  ],
+  title: 'event 2',
+  url: 'event2Url',
+  id: 2,
+};
+
+export default {
+  event1,
+  event2,
+};
