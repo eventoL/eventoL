49803214d3c7bf25ad496b216b0297ec4296f5f9
FedeG <federicogonzalez761@gmail.com>
2019-10-19 10:19:55 -0300

    #589 - Create slick bundle

---
+++ eventol/front/src/libs/slick.js
@@ -0,0 +1,31 @@
+import './slick.scss';
+
+import $ from 'jquery';
+
+import 'slick';
+import 'slick-carousel/slick/slick';
+
+const startSlick = (id, autoplay = true, dots = true) => {
+  const elementId = `#${id}`;
+  const element = $(elementId);
+
+  element.slick({
+    autoplay,
+    dots,
+  });
+
+  let isPlaying = autoplay;
+
+  const onClick = () => {
+    const action = isPlaying ? 'slickPause' : 'slickPlay';
+    isPlaying = !isPlaying;
+    element.slick('slickSetOption', 'autoplay', isPlaying).slick(action);
+  };
+
+  element.click(onClick);
+};
+
+if (!window.libs) {
+  window.libs = {};
+}
+window.libs.slick = startSlick;
