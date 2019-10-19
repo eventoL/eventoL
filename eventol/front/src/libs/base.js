5a23f9e14e596ccf07a3f0c7bdbd9133b9c52132
FedeG <federicogonzalez761@gmail.com>
2019-10-19 12:04:12 -0300

    Close #591 - Add jquery, bootstrap and material to base bundle

--- eventol/front/src/libs/base.js
+++ eventol/front/src/libs/base.js
@@ -1,1 +1,10 @@
 import './base.scss';
+
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+
+require('bootstrap/dist/js/bootstrap');
+require('bootstrap-material-design/dist/js/ripples');
+require('bootstrap-material-design/dist/js/material');
