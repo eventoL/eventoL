eae7a138065e2e93738c4e9bee2edd6679b3d94a
FedeG <federicogonzalez761@gmail.com>
2020-01-14 20:40:29 -0300

    Add tests to forms and autocomplete forms

---
+++ eventol/manager/tests/test_forms.py
@@ -0,0 +1,54 @@
+import pytest
+
+from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+
+from manager.models import Software
+from manager.forms import SoftwareAutocomplete
+
+
+def create_autocomplete_form(formclass, query, user):
+    autocomplete_form = formclass()
+    autocomplete_form.request = RequestFactory()
+    autocomplete_form.request.user = user
+    autocomplete_form.q = query
+    return autocomplete_form
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_anonymous_user(softwares):
+    software_autocomplete_form = create_autocomplete_form(
+        SoftwareAutocomplete, 'soft', AnonymousUser())
+    assert list(software_autocomplete_form.get_queryset()) == list(Software.objects.none())
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_authenticated_user_without_softwares(user1):
+    software_autocomplete_form = create_autocomplete_form(SoftwareAutocomplete, 'soft', user1)
+    assert list(software_autocomplete_form.get_queryset()) == []
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_authenticated_user_get_softwares(user1, software1, software2):
+    software_autocomplete_form = create_autocomplete_form(SoftwareAutocomplete, 'soft', user1)
+    assert list(software_autocomplete_form.get_queryset()) == [software1, software2]
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_authenticated_user_get_and_filter_softwares(user1, software1, software2):
+    software_autocomplete_form = create_autocomplete_form(SoftwareAutocomplete, 'software1', user1)
+    assert list(software_autocomplete_form.get_queryset()) == [software1]
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_authenticated_user_get_softwares_with_unaccent(user1, software1, software2):
+    software_autocomplete_form = create_autocomplete_form(SoftwareAutocomplete, 'SóFtwÅrÉ', user1)
+    assert list(software_autocomplete_form.get_queryset()) == [software1, software2]
+
+
+@pytest.mark.django_db
+def test_software_autocomplete_form_with_authenticated_user_get_only_5_softwares(user1, softwares):
+    software_autocomplete_form = create_autocomplete_form(SoftwareAutocomplete, '', user1)
+    softwares_list = list(software_autocomplete_form.get_queryset())
+    assert len(softwares_list) < len(softwares)
+    assert len(softwares_list) == 5
