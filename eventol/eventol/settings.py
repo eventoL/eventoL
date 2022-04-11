3dc204db8cb5b9282a2411167075a3ddc9dbf0fe
FedeG <federicogonzalez761@gmail.com>
2022-04-11 11:34:03 -0300

    Fix #795 - Add DONT_SET_FILE_UPLOAD_PERMISSIONS to .env.dist and refactor

--- eventol/eventol/settings.py
+++ eventol/eventol/settings.py
@@ -14,7 +14,7 @@ from easy_thumbnails.optimize.conf import OptimizeSettings
 
 
 def str_to_bool(str_bool):
-    return str_bool == 'True'
+    return str_bool.lower() == 'true'
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -176,10 +176,8 @@ class Base(Configuration):
     }
 
     CKEDITOR_UPLOAD_PATH = 'uploads/'
-    if os.getenv('DONT_SET_FILE_UPLOAD_PERMISSIONS', False):
-        FILE_UPLOAD_PERMISSIONS = None
-    else:
-        FILE_UPLOAD_PERMISSIONS = 0o644
+    DONT_SET_FILE_UPLOAD_PERMISSIONS = str_to_bool(os.getenv('DONT_SET_FILE_UPLOAD_PERMISSIONS', 'False'))
+    FILE_UPLOAD_PERMISSIONS = None if DONT_SET_FILE_UPLOAD_PERMISSIONS else 0o644
 
     AUTHENTICATION_BACKENDS = (
         # Needed to login by username in Django admin, regardless of `allauth`
